5051a8b9d3232de4c82615480041eb7515241f44
Shah Bakhat <shahbakhat008@gmail.com>
2022-12-05 17:04:37 +0000

    fiexed 2 similar ids

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const billInput = document.querySelector(".bill-input")
 const peopleInput = document.querySelector(".people-input")
-const trpPerPerson = document.getElementById("tip-amount")
+const tipPerPerson = document.getElementById("tip-amount")
 const totalPerPerson = document.getElementById("total-amount")
-const tips = document.querySelectorAll('.tips')
+const tips = document.querySelectorAll(".tips")
+
 billInput.addEventListener("input", billInputFun)
 peopleInput.addEventListener("input", peopleInputFun)
 
@@ -11,8 +12,8 @@ peopleInput.value = "1";
 tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
 tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
 
-tips.forEach(function(val
-    ){val.addEventListener("click",handleClick)})
+tips.forEach(function(val)
+{val.addEventListener("click",handleClick)})
 
 
 let billValue = 0.0;
@@ -20,23 +21,23 @@ let peopleValue = 1;
 
 function billInputFun(){
     billValue = parseFloat(billInput.value)
-    console.log(billValue)
+    calculateTip();
 }
 
 function peopleInputFun(){
     peopleValue = parseFloat(peopleInput.value)
-    calculateTip()
+    calculateTip();
 }
 
 function handleClick(event){
     tips.forEach(function(val){
         val.classList.remove("active-tip");
-        if (event.target.innerHTML == val.innerHTML){
+        if(event.target.innerHTML == val.innerHTML){
             val.classList.add("active-tip");
-            tipValue = parseFloat(val.innerHTML)/100;
+            tipValue = parseFloat(val.innerHTML)/100
         }
+        calculateTip();
     })
-   calculateTip()
 }
 
 function calculateTip(){
