544199051ea2c65bbc8fe42e28e4112a1248f5ac
Shah Bakhat <shahbakhat008@gmail.com>
2022-12-05 15:48:16 +0000

    added variables

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const billInput = document.querySelector('.bill-input')
+const peopleInput = document.querySelector('.people-input')
+const trpPerPerson = document.getElementById('tip-amount')
+const totalPerPerson = document.getElementById('total-amount')
+
+
