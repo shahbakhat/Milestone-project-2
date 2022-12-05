2326c02a982b4b46f9b7970df07b18ed917de98a
Shah Bakhat <shahbakhat008@gmail.com>
2022-12-05 15:53:43 +0000

    added new eventListeners

--- assets/script.js
+++ assets/script.js
@@ -3,4 +3,11 @@ const peopleInput = document.querySelector('.people-input')
 const trpPerPerson = document.getElementById('tip-amount')
 const totalPerPerson = document.getElementById('total-amount')
 
+billInput.addEventListener('input', billInputFun)
+peopleInput.addEventListener('input', peopleInputFun)
+
+billInput.value = "0.0";
+peopleInput.value = "1";
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
 
